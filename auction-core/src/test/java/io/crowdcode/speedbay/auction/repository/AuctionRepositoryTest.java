ed7b37e01d542a9d63203dfab1f66dbc1345de7d
Ingo Düppe <ingo.dueppe@crowdcode.io>
2017-04-05 10:22:29 +0200

    sol-06 domain model

---
+++ auction-core/src/test/java/io/crowdcode/speedbay/auction/repository/AuctionRepositoryTest.java
@@ -0,0 +1,65 @@
+package io.crowdcode.speedbay.auction.repository;
+
+import io.crowdcode.speedbay.auction.fixture.AuctionFixture;
+import io.crowdcode.speedbay.auction.model.Auction;
+import io.crowdcode.speedbay.auction.service.AuctionService;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.math.BigDecimal;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.core.IsNull.notNullValue;
+import static org.junit.Assert.assertNotNull;
+
+/**
+ * @author Ingo Düppe (Crowdcode)
+ */
+public class AuctionRepositoryTest {
+
+    private ApplicationContext context;
+
+    @Before
+    public void setup() {
+        context = new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
+    }
+
+    @Test
+    public void testApplicationContextWithIntegration() throws Exception {
+        //AuctionRepository Bean aus dem ApplicationContext laden
+
+        AuctionRepository repository = context.getBean("auctionRepository", AuctionRepository.class);
+
+        assertNotNull(repository);
+
+        //AuctionService Bean aus dem ApplicationContext laden
+
+        AuctionService service = context.getBean(AuctionService.class);
+
+        assertNotNull(service);
+
+
+        //Platziere eine Auction. Verwende AuctionFixture.buildAuction() um eine Auction zu erstellen.
+
+        Auction auction = AuctionFixture.buildAuction();
+
+        Long auctionId = service.placeAuction(
+                auction.getTitle(),
+                auction.getDescription(),
+                auction.getMinAmount());
+
+        assertThat(auctionId, is(notNullValue()));
+
+        service.bidOnAuction(auctionId, BigDecimal.valueOf(11));
+
+        Auction found = service.findAuction(auctionId);
+
+        assertThat(found.getHighestBid().getAmount().doubleValue(), is(11.0));
+    }
+
+
+
+}
--- auction-core/src/test/java/io/crowdcode/speedbay/auction/service/AuctionServiceTest.java
+++ auction-core/src/test/java/io/crowdcode/speedbay/auction/service/AuctionServiceTest.java
@@ -1,32 +1,22 @@
 package io.crowdcode.speedbay.auction.service;
 
 import org.junit.Test;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-
-import java.util.Arrays;
 
 import static org.junit.Assert.assertNotNull;
 
 /**
  * @author Ingo Düppe (Crowdcode)
  */
-public class AuctionServiceBeanTest {
+public class AuctionServiceTest {
 
     @Test
     public void testApplicationContext() throws Exception {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-
-        context.getBean(AuctionService.class);
-
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
         AuctionService auctionService = context.getBean("auctionService", AuctionService.class);
-
         assertNotNull(auctionService);
 
-        Arrays.stream(context.getBeanDefinitionNames())
-                .forEach(
-                        beanName -> System.out.println(beanName));
-
-        context.close();
-
+        ((ClassPathXmlApplicationContext)context).close();
     }
 }
