24bfbc862d2c4a911dd4de36e81e64547293b4b4
Ingo Düppe <ingo.dueppe@crowdcode.io>
2017-04-06 11:25:54 +0200

    Solution 11 - solution for LogBeanPostProcessor

---
+++ auction-core/src/main/java/io/crowdcode/speedbay/auction/util/LogBeanPostProcessor.java
@@ -0,0 +1,25 @@
+package io.crowdcode.speedbay.auction.util;
+
+import io.crowdcode.speedbay.common.AnsiColor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+
+@Slf4j
+public class LogBeanPostProcessor implements BeanPostProcessor {
+
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+        log.debug(AnsiColor.yellow("Post processing bean {} of type {} before initialization."), beanName, bean
+                .getClass().getCanonicalName());
+
+        return bean;
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        log.debug(AnsiColor.yellow("Post processing bean {} of type {} before initialization."), beanName, bean
+                .getClass().getCanonicalName());
+        return bean;
+    }
+}
