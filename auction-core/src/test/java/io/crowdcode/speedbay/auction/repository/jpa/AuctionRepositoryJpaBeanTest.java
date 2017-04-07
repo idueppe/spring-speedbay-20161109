08bec4ddb29cc258cf8f6d22198841e85d88cb2b
Ingo Düppe <ingo.dueppe@crowdcode.io>
2017-04-07 14:12:56 +0200

    Solution 20 - add Commit to test

--- auction-core/src/test/java/io/crowdcode/speedbay/auction/repository/jpa/AuctionRepositoryJpaBeanTest.java
+++ auction-core/src/test/java/io/crowdcode/speedbay/auction/repository/jpa/AuctionRepositoryJpaBeanTest.java
@@ -8,6 +8,7 @@ import io.crowdcode.speedbay.auction.repository.AuctionRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.Commit;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -28,6 +29,7 @@ public class AuctionRepositoryJpaBeanTest {
     private AuctionRepository auctionRepository;
 
     @Test
+    @Commit
     public void testAuctionCreate() throws Exception {
         Auction auction = AuctionFixture.buildDefaultAuction();
         auctionRepository.save(auction);
