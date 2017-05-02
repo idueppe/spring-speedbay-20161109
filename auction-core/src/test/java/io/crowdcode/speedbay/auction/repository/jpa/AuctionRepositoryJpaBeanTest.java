3ba69ed837d4e6916c664d419d70d80d124e621b
Ingo Düppe <ingo.dueppe@crowdcode.io>
2017-05-02 10:07:17 +0200

    fixed test

--- auction-core/src/test/java/io/crowdcode/speedbay/auction/repository/jpa/AuctionRepositoryJpaBeanTest.java
+++ auction-core/src/test/java/io/crowdcode/speedbay/auction/repository/jpa/AuctionRepositoryJpaBeanTest.java
@@ -5,8 +5,10 @@ import io.crowdcode.speedbay.auction.config.JpaPersistenceContextConfiguration;
 import io.crowdcode.speedbay.auction.fixture.AuctionFixture;
 import io.crowdcode.speedbay.auction.model.Auction;
 import io.crowdcode.speedbay.auction.repository.AuctionRepository;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.annotation.Commit;
 import org.springframework.test.context.ActiveProfiles;
@@ -23,6 +25,7 @@ import static org.junit.Assert.assertNotNull;
 @ContextConfiguration(classes = {BusinessLogicAnnotationConfiguration.class, JpaPersistenceContextConfiguration.class})
 @ActiveProfiles("jpa")
 @Transactional
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class AuctionRepositoryJpaBeanTest {
 
     @Autowired
@@ -30,10 +33,17 @@ public class AuctionRepositoryJpaBeanTest {
 
     @Test
     @Commit
-    public void testAuctionCreate() throws Exception {
+    public void test_1_AuctionCreate() throws Exception {
         Auction auction = AuctionFixture.buildDefaultAuction();
         auctionRepository.save(auction);
         assertNotNull(auction.getId());
     }
+
+    @Test
+    @Commit
+    public void test_2_FindAllAuctions() throws Exception {
+        auctionRepository.findAll().forEach(System.out::println);
+    }
+
 
 }
