2cf42d21f081e10b9f1d16d0f423f54ba865f684
Ingo Düppe <ingo.dueppe@crowdcode.io>
2017-06-20 14:44:07 +0200

    sol-04

--- auction-core/src/main/java/io/crowdcode/speedbay/auction/service/AuctionServiceBean.java
+++ auction-core/src/main/java/io/crowdcode/speedbay/auction/service/AuctionServiceBean.java
@@ -3,5 +3,13 @@ package io.crowdcode.speedbay.auction.service;
 /**
  * Created by idueppe on 20.06.17.
  */
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class AuctionServiceBean implements AuctionService{
+
+    public AuctionServiceBean() {
+        log.info("Constructor of AuctionServiceBean");
+    }
 }
