96b9ff1768da7ea99b3275a0dc3169178dbefb38
Ingo Düppe <ingo.dueppe@crowdcode.io>
2017-11-29 15:51:48 +0100

    Lösung 11 + Beispiele

--- auction-core/src/main/java/io/crowdcode/speedbay/auction/config/BusinessLogicAnnotationConfiguration.java
+++ auction-core/src/main/java/io/crowdcode/speedbay/auction/config/BusinessLogicAnnotationConfiguration.java
@@ -1,11 +1,15 @@
 package io.crowdcode.speedbay.auction.config;
 
 import io.crowdcode.speedbay.auction.model.Auction;
+import io.crowdcode.speedbay.auction.util.CachablePostProcessor;
 import io.crowdcode.speedbay.common.inmemory.InMemoryStore;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
+import org.springframework.jndi.JndiObjectFactoryBean;
+
+import javax.sql.DataSource;
 
 @Configuration
 @ComponentScan(
@@ -27,5 +31,22 @@ public class BusinessLogicAnnotationConfiguration {
         return new InMemoryStore<Auction>();
     }
 
+    @Bean
+    public static CachablePostProcessor cachablePostProcessor() {
+        return new CachablePostProcessor();
+    }
+
+    @Bean
+    public DataSource dataSourceFromJndi() {
+        // lang und breit den Code um auf JndI zuzugreifen.
+        return null;
+    }
+
+    @Bean
+    public JndiObjectFactoryBean dataSourceFromJndiFactoryBean() {
+        JndiObjectFactoryBean factoryBean = new JndiObjectFactoryBean();
+        factoryBean.setJndiName("/jndi/datasource/productDB");
+        return factoryBean;
+    }
 
 }
---
+++ auction-core/src/main/java/io/crowdcode/speedbay/auction/util/CachablePostProcessor.java
@@ -0,0 +1,69 @@
+package io.crowdcode.speedbay.auction.util;
+
+import io.crowdcode.speedbay.auction.model.Auction;
+import io.crowdcode.speedbay.auction.repository.AuctionRepository;
+import io.crowdcode.speedbay.common.AnsiColor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+
+import java.util.List;
+import java.util.Optional;
+
+@Slf4j
+public class CachablePostProcessor implements BeanPostProcessor {
+
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+        log.info(AnsiColor.red("Before Initialization of {}"), beanName);
+        return bean;
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        log.info(AnsiColor.red("After Initialization of {}"), beanName);
+        if (bean instanceof AuctionRepository) {
+            log.info(AnsiColor.red("Found AuctionRepository {}"), beanName);
+            return new CachableAuctionRepository((AuctionRepository) bean);
+        }
+        return bean;
+    }
+
+
+    public static class CachableAuctionRepository implements AuctionRepository {
+
+        private AuctionRepository target;
+
+        private List<Auction> cachedList;
+
+        public CachableAuctionRepository(AuctionRepository target) {
+            this.target = target;
+        }
+
+        @Override
+        public Optional<Auction> find(Long auctionId) {
+            return target.find(auctionId);
+        }
+
+        @Override
+        public Auction findOne(Long auctionId) {
+            return target.findOne(auctionId);
+        }
+
+        @Override
+        public List<Auction> findAll() {
+            // FIXME OMG
+            if (cachedList == null) {
+                cachedList = target.findAll();
+            }
+            return cachedList;
+        }
+
+        @Override
+        public Auction save(Auction auction) {
+            return target.save(auction);
+        }
+
+
+    }
+}
